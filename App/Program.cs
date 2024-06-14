7853399541ffe0cb6bc9e1984713ddae25d9b1ca
shadowkxris <cyafatsao@st.ug.edu.gh>
2024-06-14 21:35:59 +0000

    First Commit

---
+++ App/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace OOPDemo
+{
+    // Inheritance and Method Overriding
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inheritance and Method Overriding
+            Animal animal = new Animal();
+            animal.MakeSound();
+
+            Dog dog = new Dog();
+            dog.MakeSound();
+
+            Cat cat = new Cat();
+            cat.MakeSound();
+
+
+        }
+    }
+}
